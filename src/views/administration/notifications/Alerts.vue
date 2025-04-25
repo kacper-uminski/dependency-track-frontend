14ad513a0e8dbf9dc85382b71b9783a4a4be7279
Erik Myhrberg <erimy268@student.liu.se>
2025-04-25 09:17:02 +0200

    mini fix

--- src/views/administration/notifications/Alerts.vue
+++ src/views/administration/notifications/Alerts.vue
@@ -397,8 +397,6 @@ export default {
                   })
                   .then((response) => {
                     this.alert = response.data;
-                    this.notifySeverities = this.alert.notifySeverities; // keep severities selected after refresh
-                    console.info('Severities updated: \${JSON.stringify(this.notifySeverities)}'); // simple log
                     this.destination = this.parseDestination(this.alert);
                     this.token = this.parseToken(this.alert);
                     this.tokenHeader = this.parseTokenHeader(this.alert);
